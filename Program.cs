ef7c577d19242757b59f3b1163a63703f9557f0d
Your Name <you@example.com>
2023-09-18 19:12:36 +0300

    Задача 1 решена

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задачи находятся в ветках:
+Задача 64 - ветка main;
+Задача 66 - ветка vetka2;
+Задача 68 - ветка vetka3;
+*/
+
+Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+
+System.Console.WriteLine(PrintNumbers(1, 100));
+
+string PrintNumbers(int start, int end)
+{
+	if (start == end)
+	{
+		return start.ToString();
+	}
+	return start + " " + PrintNumbers(start+1, end);
+}
